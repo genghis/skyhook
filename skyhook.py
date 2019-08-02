c0b54c85d1e0759cca7a217e2a481e855725219a
Genghis Philip <genghisp@genghisphilipMBP.local>
2019-08-02 14:53:41 -0500

    animal githusbandry

---
+++ skyhook.py
@@ -0,0 +1,35 @@
+import requests
+import os
+
+os.system('clear')
+slackdomain = input('Please put in your slack domain (i.e.: gpff) ')
+tokenaddress = f"https://{slackdomain}.slack.com/customize/emoji"
+tokeneneed = input('\nDo you need help getting your Slack Token (y/n)? ')
+if tokeneneed == 'y':
+	os.system('clear')
+	print(f'Great. Copy this command to clipboard:\n\n\nwindow.prompt("your api token is: ",/api_token: "(.*)"/.exec(document.body.innerHTML)[1])')
+	ready = input('\n\n*Press enter to continue* -- you will need to open devtools on the page that opens and paste that command into the console')
+	os.system(f'open -a "Google Chrome" {tokenaddress}')
+else:
+	pass
+slackaddress = f'https://{slackdomain}.slack.com/api/emoji.add' #replace with the actual address for your slack instance
+TOKEN = input('\nPlease paste your Slack Token: ')
+directory = input('\nPlease put in the directory where your emoji are: ')
+if directory[-1] == "/":
+	pass
+else:
+	directory = directory+"/"
+
+def dothing():
+	files = os.listdir(directory)
+
+	for file in files:
+		imagestring = str(directory)+str(file)
+		realimage = open(imagestring, "rb")
+		cleanname = file.split('.')
+		cleanname = cleanname[0]
+		req = requests.post(url = slackaddress, data = {'token': TOKEN,'mode': 'data','name': cleanname}, files = {'image': realimage})
+		responsetext = req.text
+		print(f"Here's what we got back for {cleanname}: \n {responsetext}")
+
+dothing()
